b0ed05d2eefd834d41997b691494860db7dedf8f
Thomas Cook <tlncook85@gmail.com>
2022-05-20 09:06:17 -0600

    commit tree 444ce05ea5cc13401971c6a21ef553fb467ec788

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let totalBatteries = 0;
 
 // Code your solution here
+function addingUpAllBatteries(batteryBatches) {
+    totalBatteries = batteryBatches.reduce(function(prev, curr) {
+        return prev + curr;
+    }, totalBatteries);
+}
+
+addingUpAllBatteries(batteryBatches);
+
